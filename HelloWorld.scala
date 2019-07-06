2aa1653f38421fd8c3776a0b228fa85bd8e5cd2e
Jim Lin <jim_lin_jing@hotmail.com>
2019-07-06 16:29:58 -0400

    added HelloWorld program

---
+++ HelloWorld.scala
@@ -0,0 +1,9 @@
+object HelloWorld {
+  /* this is my first scala program
+   * this will print "Hello World" as the output
+   */
+
+  def main(args: Array[String]) {
+      println("Hello, world!") // prints Hello World
+  }
+}
